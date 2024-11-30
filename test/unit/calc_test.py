5a9ce47db68a16891c95299c188d610a2cf3b9a6
Diana Alfaro <dicaalba@gmail.com>
2024-11-30 17:53:08 -0500

    feat: update calc_test

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -1,7 +1,13 @@
 import unittest
+from unittest.mock import patch
 import pytest
+import math
 
 from app.calc import Calculator
+
+
+def mocked_validation(*args, **kwargs):
+    return True
 
 
 @pytest.mark.unit
@@ -15,9 +21,24 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.add(-2, 2))
         self.assertEqual(1, self.calc.add(1, 0))
 
+    def test_substract_method_returns_correct_result(self):
+        self.assertEqual(0, self.calc.substract(2, 2))
+        self.assertEqual(5, self.calc.substract(3, -2))
+
     def test_divide_method_returns_correct_result(self):
         self.assertEqual(1, self.calc.divide(2, 2))
         self.assertEqual(1.5, self.calc.divide(3, 2))
+
+    def test_power_method_returns_correct_result(self):
+        self.assertEqual(8, self.calc.power(2, 3))
+
+    def test_sqrt_method_returns_correct_result(self):
+        self.assertEqual(3, self.calc.sqrt(9))
+        self.assertEqual(0, self.calc.sqrt(0))
+
+    def test_log10_method_returns_correct_result(self):
+        self.assertEqual(1, self.calc.log10(10))
+        self.assertEqual(0, self.calc.log10(1))
 
     def test_add_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.add, "2", 2)
@@ -38,6 +59,23 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, 2, -0)
         self.assertRaises(TypeError, self.calc.divide, 0, 0)
         self.assertRaises(TypeError, self.calc.divide, "0", 0)
+
+    @patch('app.util.validate_permissions', side_effect=mocked_validation, create=True)
+    def test_multiply_method_returns_correct_result(self, _validate_permissions):
+        self.assertEqual(4, self.calc.multiply(2, 2))
+        self.assertEqual(0, self.calc.multiply(1, 0))
+        self.assertEqual(0, self.calc.multiply(-1, 0))
+        self.assertEqual(-2, self.calc.multiply(-1, 2))
+
+    def test_divide_method_fails_with_division_by_zero(self):
+        self.assertRaises(TypeError, self.calc.divide, 2, 0)
+
+    def test_sqrt_method_fails_with_negative_input(self):
+        self.assertRaises(TypeError, self.calc.sqrt, -1)
+
+    def test_log10_method_fails_with_non_positive_input(self):
+        self.assertRaises(TypeError, self.calc.log10, 0)
+        self.assertRaises(TypeError, self.calc.log10, -10)
 
 
 if __name__ == "__main__":  # pragma: no cover
