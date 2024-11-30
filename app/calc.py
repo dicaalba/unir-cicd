666574d5a7d653dce8083bf330080787f5418ee4
Diana Alfaro <dicaalba@gmail.com>
2024-11-30 17:51:57 -0500

    feat: update calc & api

--- app/calc.py
+++ app/calc.py
@@ -1,3 +1,9 @@
+import math
+import app
+
+class InvalidPermissions(Exception):
+    pass
+
 class Calculator:
     def add(self, x, y):
         self.check_types(x, y)
@@ -8,6 +14,9 @@ class Calculator:
         return x - y
 
     def multiply(self, x, y):
+        if not app.util.validate_permissions(f"{x} * {y}", "user1"):
+            raise InvalidPermissions('User has no permissions')
+
         self.check_types(x, y)
         return x * y
 
@@ -21,6 +30,22 @@ class Calculator:
     def power(self, x, y):
         self.check_types(x, y)
         return x ** y
+
+    def sqrt(self, x):
+        self.check_single_type(x)
+        if x < 0:
+            raise TypeError("Cannot calculate the square root of a negative number")
+        return math.sqrt(x)
+
+    def log10(self, x):
+        self.check_single_type(x)
+        if x <= 0:
+            raise TypeError("Logarithm undefined for zero or negative values")
+        return math.log10(x)
+
+    def check_single_type(self, x):
+        if not isinstance(x, (int, float)):
+            raise TypeError("Parameter must be a number")
 
     def check_types(self, x, y):
         if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
